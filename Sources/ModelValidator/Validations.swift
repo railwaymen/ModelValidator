44c5c923a01b661d8fdd9d4806dc7ebe58634f04
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-06-10 10:12:57 +0200

    Added docs in Validations struct

--- Sources/ModelValidator/Validations.swift
+++ Sources/ModelValidator/Validations.swift
@@ -5,11 +5,22 @@ public struct Validations<Model> where Model: Validatable {
     private var storage: [(PartialKeyPath<Model>, Validator<Model>)]
     
     // MARK: - Initialization
+    
+    /// Creates new Validations set for given model type.
+    ///
+    /// - Parameter model: A type of the validated model.
     public init(_ model: Model.Type = Model.self) {
         self.storage = []
     }
     
     // MARK: - Public
+    
+    /// Adds a validation.
+    ///
+    /// - Parameters:
+    ///   - keyPath: A field to be validated.
+    ///   - validator: A validator for the field.
+    ///   - error: An error which is returned if validator has failed.
     public mutating func add<T>(
         _ keyPath: KeyPath<Model, T>,
         _ validator: Validator<T>,
@@ -27,6 +38,11 @@ public struct Validations<Model> where Model: Validatable {
         ))
     }
     
+    /// Runs validations on given model.
+    ///
+    /// - Parameter model: A model to be validated.
+    ///
+    /// - Returns: An array of validation errors. If it's empty, model is valid.
     public func run(on model: Model) -> [Model.ValidationError] {
         self.storage.compactMap { _, validator in
             do {
