75c26d8a318ff3d3bd569bc1382aefeea959cc4f
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-06-10 11:06:43 +0200

    Added docs for URLValidator

--- Sources/ModelValidator/Validators/URLValidator.swift
+++ Sources/ModelValidator/Validators/URLValidator.swift
@@ -1,6 +1,23 @@
 import Foundation
 
 extension Validator where T == String {
+    
+    /// Checks if the string represents a valid URL.
+    ///
+    /// A valid URL should be a path to a file:
+    /// ```
+    /// "file:///path/to/file"
+    /// ```
+    /// Or have a scheme and a host:
+    /// ```
+    /// "scheme://host"
+    /// "https://example.com"
+    /// ```
+    /// Any other with fail e.g.:
+    /// ```
+    /// "example.com" // Will fail because has no sheme
+    /// ```
+    ///
     public static var url: Validator<T> {
         URLValidator().validator()
     }
