ea3d5f1547bf7f54c442fb82ad8578c3b51a3433
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-06-15 11:03:38 +0200

    Added validating if a sequence contains other sequence

--- Sources/ModelValidator/Extensions/Collection+Extension.swift
+++ Sources/ModelValidator/Extensions/Collection+Extension.swift
@@ -8,4 +8,29 @@ extension Collection where Element: Equatable {
         guard !endingCollection.isEmpty else { return false }
         return endingCollection.elementsEqual(collection)
     }
+
+    /// Checks if the collection contains other collection in its order.
+    ///
+    /// Code from: https://forums.swift.org/t/count-of-and-contains-other-for-collection/11245
+    @inlinable
+    public func contains<T: Collection>(_ other: T) -> Bool where T.Element == Element {
+        guard other.startIndex != other.endIndex else { return true }
+        
+        var currentMainSelfIndex = self.startIndex
+        var currentHelperSelfIndex = self.startIndex
+        var currentOtherIndex = other.startIndex
+        
+        while currentMainSelfIndex < self.endIndex {
+            while other[currentOtherIndex] == self[currentHelperSelfIndex] {
+                guard other.index(after: currentOtherIndex) != other.endIndex else { return true }
+                guard self.index(after: currentHelperSelfIndex) != self.endIndex else { return false }
+                currentHelperSelfIndex = self.index(after: currentHelperSelfIndex)
+                currentOtherIndex = other.index(after: currentOtherIndex)
+            }
+            currentMainSelfIndex = self.index(after: currentMainSelfIndex)
+            currentHelperSelfIndex = currentMainSelfIndex
+            currentOtherIndex = other.startIndex
+        }
+        return false
+    }
 }
--- Sources/ModelValidator/Validators/ContainsValidator.swift
+++ Sources/ModelValidator/Validators/ContainsValidator.swift
@@ -1,27 +1,49 @@
 import Foundation
 
 extension Validator where T: Collection, T.Element: Equatable {
-    
-    /// Checks if the validated collection contains the given element.
+    /// Checks if the validated collection contains the given elements.
     ///
-    /// - Parameter element: An element which have to be in the validated collection.
-    public static func contains(_ element: T.Element) -> Validator<T> {
-        ContainsValidator(element).validator()
+    /// - Parameter sequence: Elements which have to be in the validated collection in the given order.
+    public static func contains(_ sequence: T.Element...) -> Validator {
+        self.contains(sequence)
+    }
+    
+    /// Checks if the validated collection contains the given elements.
+    ///
+    /// - Parameter sequence: Elements which have to be in the validated collection in the given order.
+    public static func contains(_ sequence: [T.Element]) -> Validator {
+        ContainsValidator(sequence).validator()
+    }
+}
+
+extension Validator where T == String {
+    /// Checks if the validated string contains the given substring.
+    ///
+    /// - Parameter substring: A substring which has to be in the validated string.
+    public static func contains(_ substring: Substring) -> Validator {
+        self.contains(substring.map { $0 })
+    }
+    
+    /// Checks if the validated string contains the given substring.
+    ///
+    /// - Parameter substring: A substring which has to be in the validated string.
+    public static func contains(_ substring: String) -> Validator {
+        self.contains(substring.map { $0 })
     }
 }
 
 private struct ContainsValidator<T: Collection> where T.Element: Equatable {
-    private let element: T.Element
+    private let subsequence: [T.Element]
     
     // MARK: - Initialization
-    init(_ element: T.Element) {
-        self.element = element
+    init(_ subsequence: [T.Element]) {
+        self.subsequence = subsequence
     }
 }
 
 // MARK: - ValidatorType
 extension ContainsValidator: ValidatorType {
-    func validate(_ data: T) throws {
-        guard data.contains(self.element) else { throw BasicValidationError() }
+    func validate(_ sequence: T) throws {
+        guard sequence.contains(self.subsequence) else { throw BasicValidationError() }
     }
 }
