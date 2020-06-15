15ac65337d29d718cf493752a77fdad70c6b3898
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-06-15 12:45:48 +0200

    Added documentation for 'starts(with:) validators'

--- Sources/ModelValidator/Validators/StartsWithValidator.swift
+++ Sources/ModelValidator/Validators/StartsWithValidator.swift
@@ -1,21 +1,32 @@
 import Foundation
 
 extension Validator where T: Collection, T.Element: Equatable {
-    
+    /// Checks if the validated collection starts with the given sequence.
+    ///
+    /// - Parameter sequence: A sequence that has to be at the beginning of the validated collection.
     public static func starts(with sequence: T.Element...) -> Validator {
         self.starts(with: sequence)
     }
     
+    /// Checks if the validated collection starts with the given sequence.
+    ///
+    /// - Parameter sequence: A sequence that has to be at the beginning of the validated collection.
     public static func starts(with sequence: [T.Element]) -> Validator {
         StartsWithValidator(sequence).validator()
     }
 }
 
 extension Validator where T == String {
+    /// Checks if the validated string starts with the given substring.
+    ///
+    /// - Parameter substring: A substring that has to be at the beginning of the validated string.
     public static func starts(with substring: Substring) -> Validator {
         self.starts(with: substring.map { $0 })
     }
     
+    /// Checks if the validated string starts with the given substring.
+    ///
+    /// - Parameter substring: A substring has have to be at the beginning of the validated string.
     public static func starts(with substring: String) -> Validator {
         self.starts(with: substring.map { $0 })
     }
